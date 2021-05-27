ee9f2e6bb041bec7e67f4326da07c163bcb702f5
yuvytung <yuvytung@gmail.com>
2021-05-28 06:59:21 +0700

    trước khi tái cấu trúc lại project để sau này mởi rộng tốt hơn
    Signed-off-by: yuvytung <yuvytung@gmail.com>

--- src/main/webapp/app/shared/reducers/index.ts
+++ src/main/webapp/app/shared/reducers/index.ts
@@ -1,34 +1,29 @@
-import {combineReducers} from 'redux';
-import {loadingBarReducer as loadingBar} from 'react-redux-loading-bar';
+import { combineReducers } from 'redux';
+import { loadingBarReducer as loadingBar } from 'react-redux-loading-bar';
 
-import locale, {LocaleState} from './locale';
-import authentication, {AuthenticationState} from './authentication';
-import applicationProfile, {ApplicationProfileState} from './application-profile';
+import locale, { LocaleState } from './locale';
+import authentication, { AuthenticationState } from './authentication';
+import applicationProfile, { ApplicationProfileState } from './application-profile';
 
-import administration, {AdministrationState} from 'app/modules/administration/administration.reducer';
-import userManagement, {UserManagementState} from 'app/modules/administration/user-management/user-management.reducer';
-import register, {RegisterState} from 'app/modules/account/register/register.reducer';
-import activate, {ActivateState} from 'app/modules/account/activate/activate.reducer';
-import password, {PasswordState} from 'app/modules/account/password/password.reducer';
-import settings, {SettingsState} from 'app/modules/account/settings/settings.reducer';
-import passwordReset, {PasswordResetState} from 'app/modules/account/password-reset/password-reset.reducer';
+import administration, { AdministrationState } from 'app/modules/administration/administration.reducer';
+import userManagement, { UserManagementState } from 'app/modules/administration/user-management/user-management.reducer';
+import register, { RegisterState } from 'app/modules/account/register/register.reducer';
+import activate, { ActivateState } from 'app/modules/account/activate/activate.reducer';
+import password, { PasswordState } from 'app/modules/account/password/password.reducer';
+import settings, { SettingsState } from 'app/modules/account/settings/settings.reducer';
+import passwordReset, { PasswordResetState } from 'app/modules/account/password-reset/password-reset.reducer';
+import messageGroup, {
+  MessageGroupState
+} from 'app/entities/message-group/message-group.reducer';
 // prettier-ignore
-// import  { default as messageContentCustom , MessageGroupState as MessageGroupStateCustom
-// } from 'app/modules/component/chat/message/message-group/message-group.reducer';
-// prettier-ignore
-// prettier-ignore
-import messageGroup, {MessageGroupState} from 'app/entities/message-group/message-group.reducer';
-// prettier-ignore
-// prettier-ignore
-// prettier-ignore
-import messageContent, {MessageContentState} from 'app/entities/message-content/message-content.reducer';
+import messageContent, {
+  MessageContentState
+} from 'app/entities/message-content/message-content.reducer';
+/* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 // prettier-ignore
 import messageComponent, {MessageComponentState} from 'app/modules/component/chat/message/messages.reducer';
 
-/* jhipster-needle-add-reducer-import - JHipster will add reducer here */
-
-export interface IRootState
-{
+export interface IRootState {
   readonly authentication: AuthenticationState;
   readonly locale: LocaleState;
   readonly applicationProfile: ApplicationProfileState;
